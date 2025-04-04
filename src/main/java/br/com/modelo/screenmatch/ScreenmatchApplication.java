cd89c0d5f3872de2d23b65d5821ee8ecbb9f47b5
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-04 10:33:23 -0300

    commit 4 dia 04/04

--- src/main/java/br/com/modelo/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/modelo/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.modelo.screenmatch;
 
-import br.com.modelo.screenmatch.model.DadosEpisodio;
-import br.com.modelo.screenmatch.model.DadosSerie;
-import br.com.modelo.screenmatch.model.DadosTemporada;
-import br.com.modelo.screenmatch.service.ConsumoApi;
-import br.com.modelo.screenmatch.service.ConverteDados;
+import br.com.modelo.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -21,24 +14,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=fc318e3b");
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=fc318e3b");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		for (int i = 1; i<=dados.totalTemporadas(); i++){
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season="+ i +"&apikey=fc318e3b");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-		temporadas.forEach(System.out::println);
-		System.out.println("Oi");
+		Principal principal = new Principal();
+		principal.exibeMenu();
 	}
 }

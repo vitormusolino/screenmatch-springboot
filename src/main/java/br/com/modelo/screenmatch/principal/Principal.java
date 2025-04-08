081cd40eeed531e4ce375c13544bdc02921df337
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-08 09:04:57 -0300

    commit final do projeto

--- src/main/java/br/com/modelo/screenmatch/principal/Principal.java
+++ src/main/java/br/com/modelo/screenmatch/principal/Principal.java
@@ -85,5 +85,19 @@ public class Principal {
                                 "; Episódio" + e.getTitulo() +
                                 "; Data lançamento: " + e.getDataLancamento().format(formatador)
                 ));
+
+        Map<Integer, Double> avalicaoPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+        System.out.println(avalicaoPorTemporada);
+
+        DoubleSummaryStatistics est = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.summarizingDouble(Episodio::getAvaliacao));
+        System.out.println("Média: " + est.getAverage());
+        System.out.println("Melhor episódio: " + est.getMax());
+        System.out.println("Pior episódio: " + est.getMin());
+        System.out.println("Quantidade: " + est.getCount());
     }
 }
